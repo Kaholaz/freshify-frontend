3b904901000af9d434850f004eb24574fd861a5a
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-04-19 17:27:09 +0200

    feat: add non-functional top-nav and side-nav

---
+++ src/components/SideNavBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-menu default-active="2" @open="handleOpen" @close="handleClose">
+    <el-sub-menu>
+      <template #title>
+        <el-icon>
+          <HomeFilled />
+        </el-icon>
+        <span>Husholding</span>
+      </template>
+      <el-menu-item
+        v-for="household in households"
+        :key="household.id"
+        :index="`1-${household.id}`"
+      >
+        {{ household.name }}
+      </el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="1">
+      <el-icon>
+        <List />
+      </el-icon>
+      <span>Handleliste</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon>
+        <Management />
+      </el-icon>
+      <span>Oversikt</span>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon>
+        <DataAnalysis />
+      </el-icon>
+      <span>Statistikk</span>
+    </el-menu-item>
+    <el-menu-item index="4" disabled>
+      <el-icon>
+        <Dish />
+      </el-icon>
+      <span>Oppskrifter</span>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <el-icon>
+        <Setting />
+      </el-icon>
+      <span>Rediger husholdning</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import {
+  DataAnalysis,
+  Dish,
+  HomeFilled,
+  House,
+  List,
+  Location,
+  Management,
+  Setting,
+} from "@element-plus/icons-vue";
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+
+const households = [
+  {
+    id: 1,
+    name: "Hjemme",
+  },
+  {
+    id: 2,
+    name: "Arbeid",
+  },
+];
+</script>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-menu :default-active="activeIndex" mode="horizontal" :ellipsis="false" @select="handleSelect">
+    <el-menu-item index="0">LOGO</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">
+      <el-icon>
+        <User />
+      </el-icon>
+      <span>Profil</span>
+    </el-menu-item>
+    <el-button type="primary" class="menu-item-button">
+      <el-icon>
+        <TurnOff />
+      </el-icon>
+      <span>Logg ut</span>
+    </el-button>
+  </el-menu>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { Switch, TurnOff, User } from "@element-plus/icons-vue";
+
+const activeIndex = ref("1");
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+</script>
+
+<style scoped>
+.flex-grow {
+  flex-grow: 1;
+}
+
+.menu-item-button {
+  margin: auto 1rem;
+  align-content: center;
+}
+</style>
