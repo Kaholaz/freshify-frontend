73fc310df0f368983946ee2071c62c19eb729782
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2023-04-19 14:49:39 +0200

    refac: Changed prettier formatting settings

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
+import { mount } from "@vue/test-utils";
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    return true
-  })
-})
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    return true;
+  });
+});
