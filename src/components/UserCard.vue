d9db90eb5856d91dc301b79daffaa56c77c47814
JonasKos <jonas.kosmo@gmail.com>
2023-04-20 11:06:25 +0200

    chore: init

---
+++ src/components/UserCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card
+    shadow="never"
+    :body-style="{
+      padding: '30px',
+      overflow: 'hidden',
+    }"
+    class="relative m-2 w-full max-w-xl overflow-visible"
+  >
+    <h2 class="mb-5">Bruker</h2>
+    <p class="text-[#868e96]">
+      Front-end developer and open source enthusiast. We hope to develop a UI platform that can help
+      many people in the future. I still working hard to make this dream come true.
+    </p>
+    <div class="absolute -top-10 right-5">
+      <el-avatar src="https://avatars.githubusercontent.com/u/27342882?v=4" :size="80" />
+    </div>
+    <footer class="mt-2 text-right">
+      <el-link href="https://github.com/ryuhangyeong" type="primary" target="_blank">
+        Utnevn til superbruker
+      </el-link>
+    </footer>
+  </el-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.component-name-wrapper {
+}
+</style>
---
+++ src/views/HouseholdView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main id="component-name" class="component-name-wrapper">Component content</main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.component-name-wrapper {
+}
+</style>
