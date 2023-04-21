f3a85ab9e8ccb0d3c177a4cb4cf8884f946e2649
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-04-21 11:16:53 +0200

    feat: add functionality on shopping list

--- src/components/ShoppingListCard.vue
+++ src/components/ShoppingListCard.vue
@@ -1,40 +1,51 @@
 <template>
-    <el-card style="margin-bottom: 1rem;">
-        <el-row align="middle">
-            <el-col :span="2">
-                <el-checkbox size="large" class="checkbox"></el-checkbox>
-            </el-col>
-            <el-col :xs="7" :span="12">
-                <el-row>
-                    <h1>{{ props.item.type }}</h1>
-                </el-row>
-                <el-row>
-                    <el-text>Lagt til av: {{ props.item.addedBy }}</el-text>
-                </el-row>
-            </el-col>
-            <el-col :xs="15" :span="10">
-                <el-row>
-                    <div class="spacer"></div>
-                    <el-button type="primary" text>antall: {{ props.item.count }}</el-button>
-                    <el-button type="danger">slett</el-button>
-                </el-row>
-            </el-col>
+  <el-card style="margin-bottom: 1rem">
+    <el-row align="middle">
+      <el-col :span="2">
+        <el-checkbox
+          size="large"
+          :checked="item.checked"
+          :disabled="item.suggested"
+          class="checkbox"
+          @change="$emit('click', item.checked)"
+        ></el-checkbox>
+      </el-col>
+      <el-col :xs="7" :span="12">
+        <el-row>
+          <h1>{{ props.item.type.name }}</h1>
         </el-row>
-    </el-card>
+        <el-row>
+          <el-text>Lagt til av: {{ props.item.addedBy.firstName }}</el-text>
+        </el-row>
+      </el-col>
+      <el-col :xs="15" :span="10">
+        <el-row>
+          <div class="spacer"></div>
+          <el-button type="primary" text>antall: {{ props.item.count }}</el-button>
+          <el-button @click="emit('accept', item)" v-if="props.item.suggested" type="success" text
+            >godta</el-button
+          >
+          <el-button @click="emit('delete', item)" type="danger" text>slett</el-button>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 <script lang="ts" setup>
-
-import type {ShoppingListEntry} from "@/services/index";
+import type { ShoppingListEntry } from "@/services/index";
 
 const props = defineProps<{
-    item: ShoppingListEntry
-}>()
+  item: ShoppingListEntry;
+}>();
 
+const emit = defineEmits<{
+  (event: "click", args: boolean): void;
+  (event: "delete", args: ShoppingListEntry): void;
+  (event: "accept", args: ShoppingListEntry): void;
+}>();
 </script>
 <style scoped>
-
 .checkbox {
-    margin-right: 1.5rem;
-
+  margin-right: 1.5rem;
 }
 </style>
--- src/views/ShoppingListView.vue
+++ src/views/ShoppingListView.vue
@@ -1,74 +1,179 @@
 <template>
-    <h1 style="font-size: 60px;">Handleliste</h1>
-    <el-card>
-        <el-row :gutter="10">
-            <el-col :xs="12" :span="15">
-                <el-input placeholder="varetype" type="text" v-model="newItem.type" />
-            </el-col>
-            <el-col :xs="5" :span="5">
-                <el-input placeholder="antall" v-model="newItem.count" type="number" />
-            </el-col>
-            <el-col :xs="7" :span="4">
-                <el-row>
-                    <div class="spacer" />
-                    <el-button type="success">legg til</el-button>
-                </el-row>
-
-            </el-col>
-        </el-row>
-    </el-card>
-    <el-divider/>
-    <ShoppingListCard v-for="item in activeItems" :item="item" :key="item.id"></ShoppingListCard>
-    <el-row class="divider-row">
-        <el-text>Foreslåtte varer</el-text>
-        <div style="flex-grow: 1; "></div>
-        <el-button type="success" plain>Godta alle</el-button>
-        <el-button type="danger" plain>Avslå alle</el-button>
+  <h1 style="font-size: 60px">Handleliste</h1>
+  <el-card>
+    <h1>Legg til ny vare</h1>
+    <el-row>
+      <el-select v-model="newItem.type">
+        <el-option
+          v-for="type in itemTypes"
+          :key="type.id"
+          :label="type.name"
+          :value="type"
+        ></el-option>
+      </el-select>
+      <el-input
+        placeholder="antall"
+        style="max-width: 5rem; margin-left: 1rem"
+        v-model="newItem.count"
+        type="number"
+      />
+      <div class="spacer" />
+      <el-button type="success" @click="addNewItem(newItem)">legg til</el-button>
     </el-row>
-    <ShoppingListCard v-for="item in requestedItems" :item="item" :key="item.id"></ShoppingListCard>
-    <el-divider/>
-    <el-row class="divider-row">
-        <el-text>Kjøpte varer</el-text>
-        <div style="flex-grow: 1; "></div>
-        <el-button type="primary" plain>Flytt til oversikt</el-button>
-    </el-row>
-    <ShoppingListCard v-for="item in boughtItems" :item="item" :key="item.id"></ShoppingListCard>
-
+  </el-card>
+  <el-divider />
+  <ShoppingListCard
+    @click="handleClickCheckbox(item)"
+    v-for="item in Array.from(activeItems.values()).reverse()"
+    :item="item"
+    :key="item.id"
+  ></ShoppingListCard>
+  <el-divider />
+  <el-row class="divider-row">
+    <el-text>Foreslåtte varer</el-text>
+    <div style="flex-grow: 1"></div>
+    <el-button type="success" plain>Godta alle</el-button>
+    <el-button type="danger" plain>Avslå alle</el-button>
+  </el-row>
+  <ShoppingListCard
+    @click="handleClickCheckbox(item)"
+    v-for="item in Array.from(requestedItems.values()).reverse()"
+    :item="item"
+    :key="item.id"
+  ></ShoppingListCard>
+  <el-divider />
+  <el-row class="divider-row">
+    <el-text>Kjøpte varer</el-text>
+    <div style="flex-grow: 1"></div>
+    <el-button type="primary" plain>Avslutt handel</el-button>
+  </el-row>
+  <ShoppingListCard
+    @click="handleClickCheckbox(item)"
+    v-for="item in Array.from(boughtItems.values()).reverse()"
+    :item="item"
+    :key="item.id"
+  ></ShoppingListCard>
 </template>
 <script setup lang="ts">
 import ShoppingListCard from "@/components/ShoppingListCard.vue";
-import type {ShoppingListEntry} from "@/services";
-import {ref} from "vue";
+import type { ItemType, ShoppingListEntry, UserFull } from "@/services";
+import { ref } from "vue";
+import { ElMessage } from "element-plus";
+
+const itemTypes = [
+  {
+    name: "Melk",
+    id: 0,
+  } as ItemType,
+  {
+    name: "Sjokolade",
+    id: 1,
+  } as ItemType,
+];
 
 const newItem = ref({
-    type: "",
-    count: 0,
-    checked: false,
-    checkedBy: "",
-    suggested: false,
-    addedBy: "",
-}) as ShoppingListEntry;
+  type: itemTypes[1],
+  count: undefined,
+  checked: false,
+  suggested: false,
+  addedBy: { id: 3, firstName: "Arunan" } as UserFull,
+} as ShoppingListEntry);
 
-const testItem = {
-    type: "hei",
-    count: 1,
-    checked: false,
-    checkedBy: "hei",
-    suggested: true,
-    addedBy: "hei",
-};
+const activeItems = ref(new Map() as Map<string, ShoppingListEntry>);
+const requestedItems = ref(new Map() as Map<string, ShoppingListEntry>);
+const boughtItems = ref(new Map() as Map<string, ShoppingListEntry>);
 
-const items = new Map() as Map<string, number>;
-const activeItems = [testItem, testItem, testItem] as ShoppingListEntry[];
-const requestedItems = [testItem, testItem, testItem] as ShoppingListEntry[];
-const boughtItems = [testItem, testItem, testItem] as ShoppingListEntry[];
+addItem({
+  type: { name: "Melk", id: 0 },
+  count: 1,
+  checked: false,
+  suggested: false,
+  addedBy: { id: 0, firstName: "Sebastian" } as UserFull,
+  id: 0,
+} as ShoppingListEntry);
 
+addItem({
+  type: { name: "Melk", id: 0 },
+  count: 1,
+  checked: false,
+  suggested: true,
+  addedBy: { id: 1, firstName: "Eric" } as UserFull,
+  id: 1,
+});
 
+function itemToKey(item: ShoppingListEntry) {
+  return JSON.stringify({ type: item.type?.id, addedBy: item.addedBy?.id });
+}
 
+function addItem(item: ShoppingListEntry) {
+  if (item.suggested) {
+    requestedItems.value.set(itemToKey(item), item);
+  } else if (!item.checked) {
+    activeItems.value.set(itemToKey(item), item);
+  } else {
+    boughtItems.value.set(itemToKey(item), item);
+  }
+}
+
+function checkIfExistsAndUpdateCount(
+  item: ShoppingListEntry,
+  map: Map<string, ShoppingListEntry>,
+  count: number
+) {
+  if (map.has(itemToKey(item))) {
+    let previousItem = map.get(itemToKey(item));
+    previousItem.count += count;
+    map.set(itemToKey(item), previousItem);
+    return true;
+  }
+  return false;
+}
+
+function addNewItem(item: ShoppingListEntry) {
+  item.count = parseInt(item.count);
+  console.log(item.count);
+  if (checkIfExistsAndUpdateCount(item, activeItems.value, item.count)) {
+    ElMessage({
+      message: "Vare har blitt oppdatert",
+      type: "info",
+    });
+  } else if (checkIfExistsAndUpdateCount(item, requestedItems.value, item.count)) {
+    ElMessage({
+      message: "Foreslåtte vare har blitt oppdatert",
+      type: "info",
+    });
+  } else {
+    console.log("new item");
+    activeItems.value.set(itemToKey(item), {
+      ...item,
+    });
+  }
+}
+
+function handleClickCheckbox(item: ShoppingListEntry) {
+  if (item.checked) {
+    item.checked = false;
+    boughtItems.value.delete(itemToKey(item));
+    if (checkIfExistsAndUpdateCount(item, activeItems.value, item.count)) {
+      ElMessage({
+        message: "Vare har blitt oppdatert",
+        type: "info",
+      });
+    } else {
+      activeItems.value.set(itemToKey(item), item);
+    }
+    return;
+  }
+  item.checked = true;
+  activeItems.value.delete(itemToKey(item));
+  if (!checkIfExistsAndUpdateCount(item, boughtItems.value, item.count)) {
+    boughtItems.value.set(itemToKey(item), item);
+  }
+}
 </script>
 <style scoped>
 .divider-row {
-    margin-top: 0;
-    margin-bottom: 1rem;
+  margin-top: 0;
+  margin-bottom: 1rem;
 }
 </style>
