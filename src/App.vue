7cb0ccee4da4b62a3c56eb8f5f8936542d24ac44
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-04-24 11:49:16 +0200

    feat: add mobile view on navbar

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,49 @@
 import { RouterView } from "vue-router";
 import SideNavBar from "@/components/SideNavBar.vue";
 import TopNavBar from "@/components/TopNavBar.vue";
+import { computed, onMounted, ref } from "vue";
+import { useToggle } from "@vueuse/core";
+import { Expand } from "@element-plus/icons-vue";
+
+const windowSize = ref(window.innerWidth);
+
+onMounted(() => {
+  window.addEventListener("resize", () => {
+    windowSize.value = window.innerWidth;
+  });
+});
+
+const collapsed = computed(() => {
+  return windowSize.value < 768;
+});
+
+const [drawer, drawerToggle] = useToggle();
 </script>
 
 <template>
   <el-container>
     <el-header>
-      <TopNavBar />
+      <TopNavBar @menu="drawerToggle()" />
     </el-header>
+    <div v-if="collapsed">
+      <el-menu style="--el-menu-hover-bg-color: var(--el-menu-bg-color)">
+        <el-menu-item>
+          <el-button @click="drawerToggle()">
+            <el-icon>
+              <Expand />
+            </el-icon>
+            Menu
+          </el-button>
+        </el-menu-item>
+      </el-menu>
+    </div>
     <el-container>
-      <el-aside width="300px">
+      <el-aside width="300px" v-if="!collapsed">
         <SideNavBar />
       </el-aside>
+      <el-drawer v-model="drawer" direction="ltr" size="306px">
+        <SideNavBar />
+      </el-drawer>
       <el-main>
         <RouterView />
       </el-main>
--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,26 +1,36 @@
 <template>
-  <el-menu :default-active="activeIndex" mode="horizontal" :ellipsis="false" @select="handleSelect">
-    <el-menu-item index="0">LOGO</el-menu-item>
-    <div class="flex-grow" />
-    <el-menu-item index="1">
-      <el-icon>
-        <User />
-      </el-icon>
-      <span>Profil</span>
-    </el-menu-item>
-    <el-button type="primary" class="menu-item-button">
-      <el-icon>
-        <TurnOff />
-      </el-icon>
-      <span>Logg ut</span>
-    </el-button>
-  </el-menu>
+  <nav>
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="0">LOGO</el-menu-item>
+      <div class="flex-grow" />
+      <el-menu-item index="1">
+        <el-icon>
+          <User />
+        </el-icon>
+        <span>Profil</span>
+      </el-menu-item>
+      <el-button type="primary" class="menu-item-button">
+        <el-icon>
+          <TurnOff />
+        </el-icon>
+        <span>Logg ut</span>
+      </el-button>
+    </el-menu>
+  </nav>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
-import { Switch, TurnOff, User } from "@element-plus/icons-vue";
+import { TurnOff, User } from "@element-plus/icons-vue";
 
 const activeIndex = ref("1");
+const emit = defineEmits<{
+  menu: () => void;
+}>();
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
