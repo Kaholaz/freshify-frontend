166c2b6cf8a1d0f918cef3df6feee49bf75c168e
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2023-04-24 12:11:14 +0200

    refac: improve updating items in inventory

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -2,13 +2,19 @@
   <div id="inventory" class="inventory-wrapper">
     <h1>Mitt Kjøleskap</h1>
     <div class="inventory-items-list" v-loading="isLoading">
-      <ItemCard v-for="item in items" :key="item.id" :item="item" />
+      <ItemCard
+        :class="{ 'warning-age': computed(() => getDaysSinceBought(item) > 5) }"
+        v-for="item in items"
+        :key="item.id"
+        :item="item"
+      />
       <div v-if="!items && error">Ingenting å vise.</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import axios from "axios";
 import { ref, computed } from "vue";
 import { ElNotification } from "element-plus";
 
@@ -26,32 +32,69 @@ const inventoryApi = new InventoryApi();
 const householdStore = useHouseholdStore();
 
 // Define refs
-let items = ref<Item[] | null>(null); // These will be overwritten if the household id is not null
-let error = ref<Error | null>(null); // These will be overwritten if the household id is not null
+const items = ref<Item[] | null>(null);
+const error = ref<Error | null>(null);
 
 // Define computed values
 const isLoading = computed(() => items.value === null && error.value === null);
 
 // Other script logic
-const householdId = householdStore.getHousehold()?.id;
+function updateItems() {
+  items.value = null;
 
-if (householdId) {
+  let householdId = getHouseholdId();
+  if (!householdId) return;
+
   // Load inventory items
-  let { data: newItems, error: newError } = runApiRequest(
-    inventoryApi.getInventoryItems(householdId)
-  );
-  items = newItems; // eslint-disable-line
-  error = newError; // eslint-disable-line
-} else {
-  // Show an error because no household was selected
-  console.error("No household id was selected. Can not fetch inventory items.");
-  error.value = new Error("No household id was selected. Can not fetch inventory items.");
+  inventoryApi
+    .getInventoryItems(householdId)
+    .then((response) => response.data)
+    .then((data) => (items.value = data))
+    .catch(handleError);
+}
+
+function getHouseholdId(): number | null {
+  let householdId = householdStore.getHousehold()?.id;
+
+  if (!householdId) {
+    console.error("No household id was selected. Can not fetch inventory items.");
+    error.value = new Error("No household id was selected. Can not fetch inventory items.");
+
+    ElNotification({
+      title: "Ingen hjem valgt.",
+      message: "Velg et hjem for å se inventaret til hjemmet.",
+      type: "error",
+      duration: 0,
+    });
+
+    return null;
+  }
+  return householdId;
+}
+
+function handleError(err: any) {
+  console.log(err);
+  let errorMessage = "";
+
+  if (err instanceof Error) {
+    errorMessage = err.message;
+  }
+
+  if (axios.isAxiosError(err)) {
+    let { code, message } = err;
+    errorMessage = `[${code || 0}]: ${message}`;
+  }
+
   ElNotification({
-    title: "Ingen hjem valgt.",
-    message: "Velg et hjem for å se inventaret til hjemmet.",
+    title: "En feil oppstod under henting av inventaret.",
+    message: errorMessage,
     type: "error",
+    duration: 0,
   });
 }
+
+updateItems();
+// TODO: When the household is changed, update the items.
 </script>
 
 <style scoped>
