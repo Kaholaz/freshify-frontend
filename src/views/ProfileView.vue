ed847def5f23c43215c51569e881cd7c6114e27d
Eric Bieszczad-Stie <ericbi@ntnu.no>
2023-04-26 17:37:33 +0200

    feat: connect edit user to backend

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,12 +1,14 @@
 <template>
-	<EditProfile v-model="updatedUser" />
+	<EditProfile @change:user="updateUser" @change:password="updatePassword" />
 </template>
 
 <script setup lang="ts">
 import { reactive } from "vue";
+import { ElMessage } from "element-plus";
 
 import type { UpdateUser } from "@/services/index";
 import { AccountApi } from "@/services/index";
+import { showError } from "@/utils/error-utils";
 
 import EditProfile from "@/components/EditProfile.vue";
 
@@ -20,16 +22,36 @@ const updatedUser: UpdateUser = reactive({
 	password: "",
 });
 
-// Define computed values
-
 // Define callback functions
+function updateUser(firstName: string, email: string) {
+	updatedUser.firstName = firstName;
+	updatedUser.email = email;
+	sendUpdate();
+}
+
+function updatePassword(password: string) {
+	updatedUser.password = password;
+	sendUpdate();
+}
 
 // Other script logic
+function sendUpdate() {
+	accountApi
+		.updateUser(updatedUser)
+		.then(() => {
+			console.info("User updated.");
+			ElMessage({
+				message: "Brukeren din ble oppdatert.",
+				type: "success",
+			});
+		})
+		.catch(err => {
+			console.error(err);
+			showError("Noe gikk galt under lagring.", err.message);
+		});
+}
 
 </script>
 
 <style scoped>
-.profile-wrapper {
-  
-}
 </style>
