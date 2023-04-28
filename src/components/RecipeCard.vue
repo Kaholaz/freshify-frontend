b605c97926f4d201cb4e5af3c32cf45d852b2aad
JonasKos <jonas.kosmo@gmail.com>
2023-04-28 15:23:14 +0200

    chore: adds initial recipe view and recipe component

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-card
+    class="relative m-2 w-full max-w-xl overflow-visible"
+    shadow="never"
+    style="height: 11rem"
+  >
+    <h2 class="mb-5">{{ recipeTitle }}</h2>
+    <p class="text-[#868e96]">Tid: ca {{ recipeTime }} min</p>
+    <p class="text-[#868e96]">{{ recipeAmountIngredientsOwned }} ingredienser i kjøleskap</p>
+    <footer v-if="recipeAllergies.length > 0" class="mt-2 text-right">
+      <header  class="mb-5">Allergier:</header>
+      <!-- todo: formatering -->
+      <el-container><p v-for="allergy in recipeAllergies">{{ allergy }}</p></el-container>
+      
+    </footer>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import type { Ref } from "vue";
+import { ref, computed, onMounted } from "vue";
+
+// Define APIs and stores
+
+// Define props (will be recipe type)
+const props = defineProps<{
+  recipeTitle: string;
+  recipeTime: number;
+  recipeAmountIngredientsOwned: number;
+  recipeAllergies: string[];
+}>();
+
+
+
+// Define emits
+const emit = defineEmits<{
+  (event: "some-event", ...args: any[]): void;
+}>();
+
+// Define refs
+const value = ref(null) as Ref<HTMLElement | null>;
+
+// Define computed values
+const computedValue = computed(() => {});
+
+// Define callback functions
+function onClick() {}
+
+// Vue hooks
+onMounted(() => {});
+
+// Other script logic
+</script>
+
+<style scoped>
+.recipe-card-wrapper {
+}
+</style>
--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,3 +1,69 @@
 <template>
-  <h1>Recipe View</h1>
+  <div>
+    <h1>Oppskrifter</h1>
+    <el-row :gutter="10" style="width: 100%; margin: 0">
+      <el-col
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        :lg="12"
+        :md="24"
+        :sm="24"
+        :xl="12"
+        :xs="24"
+      >
+        <RecipeCard
+          :recipe-title="recipe.recipeTitle"
+          :recipe-time="recipe.recipeTime"
+          :recipe-amount-ingredients-owned="recipe.recipeAmountIngredientsOwned"
+          :recipe-allergies="recipe.recipeAllergies"
+        />
+      </el-col>
+    </el-row>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import RecipeCard from "@/components/RecipeCard.vue";
+
+//test recipes
+const recipes = {
+  taco: {
+    id: 1,
+    recipeTitle: "Taco",
+    recipeTime: 30,
+    recipeAmountIngredientsOwned: 5,
+    recipeAllergies: ["melk", "egg", "gluten"],
+  },
+  pizza: {
+    id: 2,
+    recipeTitle: "Pizza",
+    recipeTime: 40,
+    recipeAmountIngredientsOwned: 3,
+    recipeAllergies: ["gluten"],
+  },
+  pasta: {
+    id: 3,
+    recipeTitle: "Pasta",
+    recipeTime: 20,
+    recipeAmountIngredientsOwned: 7,
+    recipeAllergies: ["egg", "gluten"],
+  },
+  burger: {
+    id: 4,
+    recipeTitle: "Burger",
+    recipeTime: 30,
+    recipeAmountIngredientsOwned: 2,
+    recipeAllergies: ["gluten"],
+  },
+  sushi: {
+    id: 5,
+    recipeTitle: "Sushi",
+    recipeTime: 60,
+    recipeAmountIngredientsOwned: 1,
+    recipeAllergies: [],
+  },
+}
+
+</script>
+
+<style scoped></style>
