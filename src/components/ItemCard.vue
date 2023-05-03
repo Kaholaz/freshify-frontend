ee6a96ad8dddbff05ec7d1a5e71da1c130b585e4
Eric Bieszczad-Stie <ericbi@ntnu.no>
2023-05-03 11:34:31 +0200

    chore: run formatter

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -4,7 +4,7 @@
     :class="{
       'item-card-wrapper': true,
       'item-card-only-title': !item.addedBy,
-      'warning-age': (getDaysSinceLastChanged(item) > 7 && getDaysSinceLastChanged(item) <= 14),
+      'warning-age': getDaysSinceLastChanged(item) > 7 && getDaysSinceLastChanged(item) <= 14,
       'danger-age': getDaysSinceLastChanged(item) > 14,
     }"
     shadow="always"
