688c13444f34e2236a8c98d3caba00e74cc7b08f
Navid <Navidm@stud.ntnu.no>
2023-05-05 18:52:12 +0200

    fix: made getDaysSinceLastChanged return a computed in utils

--- src/utils/item-utils.ts
+++ src/utils/item-utils.ts
@@ -1,4 +1,5 @@
 import type { Item } from "@/services/index";
+import { computed } from "vue";
 
 export function getDaysSinceBought(item: Item): number {
   if (!item.bought) return NaN;
@@ -7,9 +8,11 @@ export function getDaysSinceBought(item: Item): number {
   return Math.floor(timeDelta / (1000 * 60 * 60 * 24));
 }
 
-export function getDaysSinceLastChanged(item: Item): number {
-  if (!item.lastChanged) return NaN;
+export function getDaysSinceLastChanged(item: Item) {
+  return computed(() => {
+    if (!item.lastChanged) return NaN;
 
-  const timeDelta = Date.now() - Date.parse(item.lastChanged);
-  return Math.floor(timeDelta / (1000 * 60 * 60 * 24));
+    const timeDelta = Date.now() - Date.parse(item.lastChanged);
+    return Math.floor(timeDelta / (1000 * 60 * 60 * 24));
+  });
 }
