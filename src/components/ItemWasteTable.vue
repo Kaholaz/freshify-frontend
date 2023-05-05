c9c3a9e6e7d05e2a9635a50becfdcb3e1d3d89f7
Mats Lading <mjlading@stud.ntnu.no>
2023-05-05 13:01:28 +0200

    chore: format

--- src/components/ItemWasteTable.vue
+++ src/components/ItemWasteTable.vue
@@ -1,7 +1,12 @@
 <template>
   <el-table :data="props.tableData" style="width: 100%" height="250" default-sort="amountWasted">
     <el-table-column fixed prop="name" label="Vare" sortable />
-    <el-table-column prop="amountWasted" label="svinn per vare (%)" sortable :formatter="(row: any) => Math.trunc((Number(row.amountWasted)*100))"/>
+    <el-table-column
+      prop="amountWasted"
+      label="svinn per vare (%)"
+      sortable
+      :formatter="(row: any) => Math.trunc((Number(row.amountWasted)*100))"
+    />
     <el-table-column prop="countWasted" label="totalt" sortable />
   </el-table>
 </template>
--- src/components/WastedItemList.vue
+++ src/components/WastedItemList.vue
@@ -1,29 +1,27 @@
 <template>
-    <el-card id="edit-profile" class="edit-profile-wrapper">
-        <h2>
-            Mest svinnede varer
-        </h2>
-        <div style="margin-top: 20px">
-            <el-radio-group v-model="radio">
-                <el-radio-button label="Antall svunnet" />
-                <el-radio-button label="Andel svunnet (%)" />
-            </el-radio-group>
-        </div>
-        <el-table :data="tableDataWastedItemsByCount" style="width: 100%">
-            <el-table-column type="index" width="50" />
-            <el-table-column prop="itemType" label="Vare" />
-            <el-table-column prop="amountWasted" label="Antall svunnet" />
-        </el-table>
-    </el-card>
+  <el-card id="edit-profile" class="edit-profile-wrapper">
+    <h2>Mest svinnede varer</h2>
+    <div style="margin-top: 20px">
+      <el-radio-group v-model="radio">
+        <el-radio-button label="Antall svunnet" />
+        <el-radio-button label="Andel svunnet (%)" />
+      </el-radio-group>
+    </div>
+    <el-table :data="tableDataWastedItemsByCount" style="width: 100%">
+      <el-table-column type="index" width="50" />
+      <el-table-column prop="itemType" label="Vare" />
+      <el-table-column prop="amountWasted" label="Antall svunnet" />
+    </el-table>
+  </el-card>
 </template>
-  
+
 <script setup lang="ts">
 import { ref } from "vue";
 import type { Item } from "@/services/index";
 import { InventoryApi } from "@/services/index";
 import { useHouseholdStore } from "@/stores/household";
 
-const radio = ref('Antall svunnet')
+const radio = ref("Antall svunnet");
 const items = ref<Item[] | null>(null);
 
 // Defines stores
@@ -33,13 +31,12 @@ const householdStore = useHouseholdStore();
 const inventoryApi = new InventoryApi();
 
 function updateItems() {
-    items.value = null;
+  items.value = null;
 
-    let householdId = getHouseholdId();
-    if (householdId == null) return;
+  let householdId = getHouseholdId();
+  if (householdId == null) return;
 
-    // Load inventory items
-
+  // Load inventory items
 }
 
 function getHouseholdId(): number | null {
@@ -55,59 +52,54 @@ function getHouseholdId(): number | null {
 }
 
 const tableDataWastedItemsByCount = [
-    {
-        itemType: 'Melk',
-        amountWasted: 9,
-    },
-    {
-        itemType: "Brød",
-        amountWasted: 4,
-    }
-]
+  {
+    itemType: "Melk",
+    amountWasted: 9,
+  },
+  {
+    itemType: "Brød",
+    amountWasted: 4,
+  },
+];
 
 const tableDataWastedItemsByAverageAmount = [
-    {
-        itemType: 'Melk',
-        amountWasted: 0.8,
-    },
-    {
-        itemType: "Brød",
-        amountWasted: 0.5,
-    }
-]
+  {
+    itemType: "Melk",
+    amountWasted: 0.8,
+  },
+  {
+    itemType: "Brød",
+    amountWasted: 0.5,
+  },
+];
 
 // Define APIs and store
-
 
 // Define emits
 
 // Define refs
 
-
 const rules = ref({
-    firstName: [
-        { required: true, message: "Fornavn er påkrevd", trigger: "blur" },
-        { min: 2, message: "Fornavn må være minst 2 tegn", trigger: "blur" },
-    ],
-    email: [
-        { required: true, message: "Email er påkrevd", trigger: "blur" },
-        { type: "email", message: "Email må være en gyldig email", trigger: "blur" },
-    ],
+  firstName: [
+    { required: true, message: "Fornavn er påkrevd", trigger: "blur" },
+    { min: 2, message: "Fornavn må være minst 2 tegn", trigger: "blur" },
+  ],
+  email: [
+    { required: true, message: "Email er påkrevd", trigger: "blur" },
+    { type: "email", message: "Email må være en gyldig email", trigger: "blur" },
+  ],
 });
 
 // Define callback functions
 function validatePasswordConfirm() {
-    if (passwordData.value.password !== passwordData.value.passwordConfirm) {
-        return Promise.reject("Passordene må være like!");
-    } else {
-        return Promise.resolve();
-    }
+  if (passwordData.value.password !== passwordData.value.passwordConfirm) {
+    return Promise.reject("Passordene må være like!");
+  } else {
+    return Promise.resolve();
+  }
 }
 
-
 // Other script logic
-
 </script>
-  
+
 <style scoped></style>
-  
